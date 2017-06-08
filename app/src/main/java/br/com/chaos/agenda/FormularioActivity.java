b586375037a87203def979fe7ba4efad01228cc0
Gerson Reis <gersonapollo@gmail.com>
2017-06-08 16:43:15 -0300

    Fontes criados durante o curso da Alura de Android

---
+++ app/src/main/java/br/com/chaos/agenda/FormularioActivity.java
@@ -0,0 +1,29 @@
+package br.com.chaos.agenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import javax.xml.datatype.Duration;
+
+public class FormularioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+
+
+        Button botaoSalvar = (Button) findViewById(R.id.formulario_salvar);
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FormularioActivity.this, "Botao Clicado", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
