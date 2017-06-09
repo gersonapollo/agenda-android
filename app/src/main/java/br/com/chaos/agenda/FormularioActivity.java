be4595dab69186ab8f564934c55347e7f4b9fe1a
Gerson Reis <gersonapollo@gmail.com>
2017-06-08 22:48:50 -0300

    Alterando o formulario, utilizando o botao no menu

--- app/src/main/java/br/com/chaos/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/chaos/agenda/FormularioActivity.java
@@ -2,9 +2,14 @@ package br.com.chaos.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.util.zip.Inflater;
 
 import javax.xml.datatype.Duration;
 
@@ -16,14 +21,26 @@ public class FormularioActivity extends AppCompatActivity {
         setContentView(R.layout.activity_formulario);
 
 
-        Button botaoSalvar = (Button) findViewById(R.id.formulario_salvar);
-        botaoSalvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_formulario, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_formulario_ok:
                 Toast.makeText(FormularioActivity.this, "Botao Clicado", Toast.LENGTH_SHORT).show();
-            }
-        });
+                finish();
+                break;
+        }
 
-
+        return super.onOptionsItemSelected(item);
     }
 }
