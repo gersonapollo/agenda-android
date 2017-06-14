2c10f407905adedaacb16212da216421ac26b9b9
Gerson Reis <gersonapollo@gmail.com>
2017-06-14 01:26:09 -0300

    acessando a camera

--- app/src/main/java/br/com/chaos/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/chaos/agenda/FormularioActivity.java
@@ -1,6 +1,15 @@
 package br.com.chaos.agenda;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Parcelable;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,8 +18,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.text.Normalizer;
 import java.util.zip.Inflater;
 
 import javax.xml.datatype.Duration;
@@ -21,7 +33,9 @@ import br.com.chaos.agenda.model.Aluno;
 
 public class FormularioActivity extends AppCompatActivity {
 
+    public static final int CAMERA_CODE = 555;
     private FormularioHelper helper;
+    private String caminhoFoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +49,42 @@ public class FormularioActivity extends AppCompatActivity {
         if(aluno != null){
             helper.preencherFormulario(aluno);
         }
+
+        Button botao = (Button) findViewById(R.id.formulario_botao_foto);
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ActivityCompat.checkSelfPermission(FormularioActivity.this, Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(FormularioActivity.this, new String[]{Manifest.permission.CAMERA}, 999);
+                }
+
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                caminhoFoto = getExternalFilesDir(null) + "/" + System.currentTimeMillis() + ".jpg";
+                File arquivo = new File(caminhoFoto);
+
+                Uri uri = Uri.fromFile(arquivo);
+
+                intent.putExtra(Intent.EXTRA_STREAM, uri);
+                startActivityForResult(intent, CAMERA_CODE);
+
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == Activity.RESULT_OK && requestCode == CAMERA_CODE){
+            ImageView foto = (ImageView) findViewById(R.id.formulario_foto);
+            Bitmap fotoBmp = BitmapFactory.decodeFile(caminhoFoto);
+            Bitmap bitmapReduzido = Bitmap.createScaledBitmap(fotoBmp, 300, 300, true);
+            foto.setImageBitmap(bitmapReduzido);
+            foto.setScaleType(ImageView.ScaleType.FIT_XY);
+
+        }
+
 
     }
 
